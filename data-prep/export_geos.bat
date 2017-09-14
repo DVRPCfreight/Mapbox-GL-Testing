e1840499ac11b46aa4ed0075d3d1c0a5dbfea218
DVRPCfreight <mruane@dvrpc.org>
2017-09-13 22:56:09 -0400

    Modify processing config BAT

--- data-prep/export_geos.bat
+++ data-prep/export_geos.bat
@@ -1,3 +1,5 @@
-@echo off
 
-python postgis2geojson.py -o freeways -H mario -d postgres -t tim23_link -w '"TYPENO" = "11" OR "TYPENO" = "12" OR "TYPENO" = "13" OR "TYPENO" = "85" OR "TYPENO" = "92"' -f gid no typeno numlanes -u postgres -p sergt --topojson
+echo: "Exporting freeways.."
+python postgis2geojson.py -o freeways -H mario -d postgres -u postgres -p sergt -t tim23_link -f gid no typeno numlanes -u postgres -p sergt -w "typeno = '11' OR typeno = '12' OR typeno = '13' OR typeno = '85' OR typeno = '92'"
+
+echo: "Exporting parkways.."
