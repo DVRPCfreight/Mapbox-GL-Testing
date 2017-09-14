8a6f452b59568aae5f9c9e566f00ec6b053c386b
Michael Ruane <mruane@dvrpc.org>
2017-09-13 23:01:25 -0400

    Revised export processing

--- data-prep/export_geos.bat
+++ data-prep/export_geos.bat
@@ -1,5 +1,19 @@
+@echo off
 
-echo: "Exporting freeways.."
+echo Exporting freeways..
 python postgis2geojson.py -o freeways -H mario -d postgres -u postgres -p sergt -t tim23_link -f gid no typeno numlanes -u postgres -p sergt -w "typeno = '11' OR typeno = '12' OR typeno = '13' OR typeno = '85' OR typeno = '92'"
 
-echo: "Exporting parkways.."
+echo Exporting parkways and majors..
+python postgis2geojson.py -o major_arts -H mario -d postgres -u postgres -p sergt -t tim23_link -f gid no typeno numlanes -u postgres -p sergt -w "typeno = '21' OR typeno = '22' OR typeno = '31' OR typeno = '32' OR typeno = '33'"
+
+echo Exporting minor arterials..
+python postgis2geojson.py -o minor_arts -H mario -d postgres -u postgres -p sergt -t tim23_link -f gid no typeno numlanes -u postgres -p sergt -w "typeno = '23' OR typeno = '41' OR typeno = '42' OR typeno = '43'"
+
+echo Exporting major collectors..
+python postgis2geojson.py -o major_cols -H mario -d postgres -u postgres -p sergt -t tim23_link -f gid no typeno numlanes -u postgres -p sergt -w "typeno = '51' OR typeno = '52' OR typeno = '53' OR typeno = '81' OR typeno = '82' OR typeno = '83' OR typeno = '86'"
+
+echo Exporting minor collectors..
+python postgis2geojson.py -o minor_cols -H mario -d postgres -u postgres -p sergt -t tim23_link -f gid no typeno numlanes -u postgres -p sergt -w "typeno = '61' OR typeno = '62' OR typeno = '63'"
+
+echo Exporting locals..
+python postgis2geojson.py -o locals -H mario -d postgres -u postgres -p sergt -t tim23_link -f gid no typeno numlanes -u postgres -p sergt -w "typeno = '71' OR typeno = '72' OR typeno = '73' OR typeno = '75' OR typeno = '76'"
--- data-prep/query.py
+++ data-prep/query.py
@@ -2,20 +2,20 @@
 # queries for heirarchy
 
 ## find all freeways and toll plazas
-WHERE 'typeno' = '11' OR 'TYPENO' = '12' OR 'TYPENO' = '13' OR 'TYPENO' = '85' OR 'TYPENO' = '92'
+WHERE typeno = '11' OR typeno = '12' OR typeno = '13' OR typeno = '85' OR typeno = '92'
 
 ## parkways and majors
-WHERE 'TYPENO' = '21' OR 'TYPENO' = '22' OR 'TYPENO' = '31' OR 'TYPENO' = '32' OR 'TYPENO' = '33'
+WHERE typeno = '21' OR typeno = '22' OR typeno = '31' OR typeno = '32' OR typeno = '33'
 
 ## low cap parkways and minor arterial
-WHERE 'TYPENO' = '23' OR 'TYPENO' = '41' OR 'TYPENO' = '42' OR 'TYPENO' = '43' 
+WHERE typeno = '23' OR typeno = '41' OR typeno = '42' OR typeno = '43' 
 
 ## major collector + ramps
-WHERE 'TYPENO' = '51' OR 'TYPENO' = '52' OR 'TYPENO' = '53' OR 'TYPENO' = '81' OR 'TYPENO' = '82' OR 'TYPENO' = '83' OR 'TYPENO' = '86'  
+WHERE typeno = '51' OR typeno = '52' OR typeno = '53' OR typeno = '81' OR typeno = '82' OR typeno = '83' OR typeno = '86'  
 
 ## minor collector
-WHERE 'TYPENO' = '61' OR 'TYPENO' = '62' OR 'TYPENO' = '63'
+WHERE typeno = '61' OR typeno = '62' OR typeno = '63'
 
 ## locals, driveways, and mall roads
-WHERE 'TYPENO' = '71' OR 'TYPENO' = '72' OR 'TYPENO' = '73' OR 'TYPENO' = '75' OR 'TYPENO' = '76'
+WHERE typeno = '71' OR typeno = '72' OR typeno = '73' OR typeno = '75' OR typeno = '76'
 
